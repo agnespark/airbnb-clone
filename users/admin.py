7d83d93ed4b118f098d252f5289905a8c2e4d0b3
kyuun <96134745+plily93@users.noreply.github.com>
2022-06-29 23:40:45 +0900

    [Fix] Users Admin: create new fieldsets

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
-@admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
- 
+@admin.register(models.User) # = admin.site.register(models.User, CustomrUserAdmin)
+class CustomUserAdmin(UserAdmin):
+    
+    """ Custom User Admin """
+
+    fieldsets = UserAdmin.fieldsets + (("Custom Profile", {"fields": ("avatar", "gender", "bio", "birthdate", "language", "currency", "superhost")}),)
+
