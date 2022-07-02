afa30be06024664107de9b05adbdb65e63930474
kyuun <agnespark0413@gmail.com>
2022-07-02 23:00:31 +0900

    <Fix> add abstract model as Abstractitem, make MTM relation, add def __str__ in Room model, add admin.py
    1. add abstract model : Abstractitem
    2. ManytoMany field Abstractitem <> Roomtype model
    3. add def __str__ in Room model
    4. add content in admin.py

--- rooms/models.py
+++ rooms/models.py
@@ -1,8 +1,46 @@
+from statistics import mode
 from django.db import models
+from django_countries.fields import CountryField
 from core import models as core_models
+from users import models as user_models
+
+class AbstractItem(core_models.TimeStampedModel):
+
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+
+    pass
+
 
 class Room(core_models.TimeStampedModel):
 
     """ Room Model Definition """
 
-    pass
+    name = models.CharField(max_length=140, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    country = CountryField(null=True, blank=True)
+    city = models.CharField(max_length=80, null=True, blank=True)
+    price = models.IntegerField(null=True, blank=True)
+    address = models.CharField(max_length=140, null=True, blank=True)
+    guests = models.IntegerField(null=True, blank=True)
+    beds = models.IntegerField(null=True, blank=True)
+    bedrooms =  models.IntegerField(null=True, blank=True)
+    baths = models.IntegerField(null=True, blank=True)
+    check_in = models.TimeField(null=True, blank=True)
+    check_out = models.TimeField(null=True, blank=True)
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True, blank=True)
+    room_type = models.ManyToManyField(RoomType, blank=True)
+
+    def __str__(self):
+        return self.name
