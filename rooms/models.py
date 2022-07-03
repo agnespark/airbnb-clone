19564f7149263148efa6408fa5d9957d3453063b
kyuun <agnespark0413@gmail.com>
2022-07-03 15:17:18 +0900

    <Fix> add photo model in rooms app
    1. add photo model in rooms app
    2. add class Meta to use 'verbose_name_plural' in each of RoomType, Amenity, Facility, Houserule model
    3. add clss PhotoAdmin in rooms' admin.py

--- rooms/models.py
+++ rooms/models.py
@@ -20,22 +20,42 @@ class AbstractItem(core_models.TimeStampedModel):
 class RoomType(AbstractItem):
 
     """ RoomType Object Definition """
-    pass
+    class Meta:
+        verbose_name = "Room Type"
+        ordering = ['name']
 
 class Amenity(AbstractItem):
 
     """ Amenity Object Definition """
-    pass
+
+    class Meta:
+        verbose_name_plural = "Amenities"
 
 class Facility(AbstractItem):
 
     """ Facility Model Definition """
-    pass
+
+    class Meta:
+        verbose_name_plural = "Facilities"
 
 class HouseRule(AbstractItem):
 
     """ HouseRule Model Definition """
-    pass
+
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+
+    """ Photo  Model Definition """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
 
 
 class Room(core_models.TimeStampedModel):
@@ -55,11 +75,11 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField(null=True, blank=True)
     check_out = models.TimeField(null=True, blank=True)
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True, blank=True)
-    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
-    amenities = models.ManyToManyField(Amenity)
-    facilities = models.ManyToManyField(Facility)
-    houserule = models.ManyToManyField(HouseRule)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE, null=True, blank=True)
+    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True, blank=True)
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
+    houserule = models.ManyToManyField("HouseRule", blank=True)
 
     def __str__(self):
         return self.name
