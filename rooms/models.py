f9ed10289aea12f9b17649d30ef9e53ee9afebc4
plily93 <agnespark0413@gmail.com>
2022-08-21 16:12:10 +0900

    #9.4 seed_rooms

--- rooms/models.py
+++ rooms/models.py
@@ -105,11 +105,8 @@ class Room(core_models.TimeStampedModel):
 
     def total_rating(self):
         all_reviews = self.reviews.all()
-        all_ratings = 5
-        for review in all_reviews:
-            all_ratings += review.rating_average()
-
+        all_ratings = 0
         if len(all_reviews) > 0:
+            for review in all_reviews:
+                all_ratings += review.rating_average()
             return round((all_ratings / len(all_reviews)), 2)
-        else:
-            return 0
