958408fde9395c7d0207712c56b810a67fa35544
plily93 <agnespark0413@gmail.com>
2022-08-27 14:54:50 +0900

    #11.5 get_page vs page

--- rooms/views.py
+++ rooms/views.py
@@ -4,17 +4,18 @@ from django.core.paginator import Paginator
 from . import models
 
 
+# 2. use paginator
 def all_rooms(request):
     page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    return render(request, "rooms/home.html", {"rooms": rooms})
+    room_list = models.Room.objects.all()  # 그냥 쿼리셋을 생성할 뿐, 즉시 호출하지는 않음
+    paginator = Paginator(room_list, 10, orphans=5)  # 페이지당 리스트 갯수 = 10
+    rooms = paginator.page(int(page))
+    return render(request, "rooms/home.html", {"page": rooms})
 
 
 # 1. pagination 하드코딩방법
 # def all_rooms(request):
-#     page = request.GET.get("page", 1)
+#     page = request.GET.get("page", 1) #page가 없다면 기본값으로 1을 넘겨라
 #     page = int(page or 1)
 #     page_size = 10
 #     limit = page_size * page
